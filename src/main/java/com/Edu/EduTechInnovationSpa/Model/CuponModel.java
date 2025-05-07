02106d2cf51adfb8697a98230f91d690f826af14
bastian <bas.cares@duocuc.cl>
2025-05-07 18:18:33 -0400

    Curso-Model-v1 + cambios de nombres

--- src/main/java/com/Edu/EduTechInnovationSpa/Model/CuponModel.java
+++ src/main/java/com/Edu/EduTechInnovationSpa/Model/CuponModel.java
@@ -13,7 +13,7 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Cupon {
+public class CuponModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/main/java/com/Edu/EduTechInnovationSpa/Model/CursoModel.java
@@ -0,0 +1,39 @@
+package com.Edu.EduTechInnovationSpa.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+
+@Entity
+@Table(name = "curso")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CursoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_curso;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "descripcion", nullable = false)
+    private String descripcion;
+
+    @Column(name = "docente", nullable = false)
+    private String docente;
+
+    @Column(name = "fecha_inicio", nullable = false)
+    private Date fecha_inicio;
+
+    @Column(name = "fecha_termino", nullable = false)
+    private Date fecha_termino;
+
+    @Column(name = "valor", nullable = false)
+    private float valor;
+}
