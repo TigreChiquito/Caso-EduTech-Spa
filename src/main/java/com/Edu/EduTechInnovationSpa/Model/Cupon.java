c164de52e1840c3c0fdf0fd264abd5be1e748324
bastian <bas.cares@duocuc.cl>
2025-05-07 18:10:41 -0400

    Model-Cupon-v1

---
+++ src/main/java/com/Edu/EduTechInnovationSpa/Model/Cupon.java
@@ -0,0 +1,39 @@
+package com.Edu.EduTechInnovationSpa.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+
+@Entity
+@Table(name = "cupon")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_cupon;
+
+    @Column(name = "code", unique = true, nullable = false)
+    private String code;
+
+    @Column(name = "discount", nullable = false)
+    private float discount;
+
+    @Column(name = "start_date", nullable = false)
+    private Date start_date;
+
+    @Column(name = "end_date", nullable = false)
+    private Date end_date;
+
+    @Column(name = "use_limit", nullable = false)
+    private Integer use_limit;
+
+    @Column(name = "used", nullable = false)
+    private Integer used;
+}
