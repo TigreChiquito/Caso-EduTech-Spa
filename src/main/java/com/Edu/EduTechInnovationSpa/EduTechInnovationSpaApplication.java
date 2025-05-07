ed5972b6e58bb8e2daa950a032d1be2c881df1d1
bastian <bas.cares@duocuc.cl>
2025-05-07 15:28:58 -0400

    first commit

---
+++ src/main/java/com/Edu/EduTechInnovationSpa/EduTechInnovationSpaApplication.java
@@ -0,0 +1,13 @@
+package com.Edu.EduTechInnovationSpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduTechInnovationSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduTechInnovationSpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Edu/EduTechInnovationSpa/EduTechInnovationSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Edu.EduTechInnovationSpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduTechInnovationSpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
