dfaca8f23e976f2d74c5cb2944e606987eccc83e
lobosvalenzuela <t.lobos@duocuc.cl>
2025-05-11 19:05:15 -0400

    #ControllerRolUser

--- src/main/java/com/Edu/EduTechInnovationSpa/Controller/EduTechController.java
+++ src/main/java/com/Edu/EduTechInnovationSpa/Controller/EduTechController.java
@@ -2,6 +2,7 @@ package com.Edu.EduTechInnovationSpa.Controller;
 
 import com.Edu.EduTechInnovationSpa.Model.Cupon;
 import com.Edu.EduTechInnovationSpa.Model.Curso;
+import com.Edu.EduTechInnovationSpa.Model.RolUsuario;
 import com.Edu.EduTechInnovationSpa.Model.Usuario;
 import com.Edu.EduTechInnovationSpa.Service.*;
 
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/v1/edu")
@@ -26,6 +29,9 @@ public class EduTechController {
 
     @Autowired
     private CuponService cuponService;
+
+    @Autowired
+    private RolUsuarioService rolUsuarioService;
 
     // - - - - - - - - - - Controladores de Usuarios - - - - - - - - - -
 
@@ -106,10 +112,58 @@ public class EduTechController {
         return ResponseEntity.ok(cupones);
     }
 
-    // Controlador RolUsuario
-    @GetMapping("")
-    public String getMethodName(@RequestParam String param) {
-        return new String();
+    // --------Controlador RolUsuario
+
+    @GetMapping("/RolUsuario")
+    public ResponseEntity<List<RolUsuario>> ListarRoles() {
+        List<RolUsuario> rolUsuarios = rolUsuarioService.getAllUsers();
+        if (rolUsuarios.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(rolUsuarios);
+
+    }
+
+    @GetMapping("/RolUsuario/{id}")
+    public ResponseEntity<RolUsuario> obtenerRolUsuario(@PathVariable Integer id) {
+        RolUsuario rolUsuario = rolUsuarioService.getRolById(id);
+        if (rolUsuario == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(rolUsuario);
+    }
+
+    @PostMapping("/RolUsuario")
+    public ResponseEntity<RolUsuario> CrearRol(@RequestBody RolUsuario rolUsuario) {
+        RolUsuario nuevoRolUsuario = rolUsuarioService.createRolUsuario(rolUsuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevoRolUsuario);
+    }
+
+    @PostMapping("/RolUsuario/{id}")
+    public ResponseEntity<RolUsuario> ActualizarRol(@PathVariable Integer id, @RequestBody RolUsuario rolUsuario) {
+        try {
+
+            RolUsuario rolUser = rolUsuarioService.getRolById(id);
+
+            rolUser.setId_rol(rolUsuario.getId_rol());
+            rolUser.setTipo_rol(rolUsuario.getTipo_rol());
+            rolUser.setDescripcion_rol(rolUsuario.getDescripcion_rol());
+            RolUsuario rolActualizado = rolUsuarioService.createRolUsuario(rolUser);
+            return ResponseEntity.ok(rolActualizado);
+
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/RolUsuario/{id}")
+    public ResponseEntity<?> eliminarRol(@PathVariable Integer id) {
+        try {
+            rolUsuarioService.deleteRolUsuario(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
